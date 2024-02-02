b03b41b551f58d4c2fd78416fbd6b1887cb300d4
Minh Nguyen <minh.nguyen2810@hcmut.edu.vn>
2024-02-02 14:33:51 +0700

    Update subcribe loop

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import random
 
 AIO_FEED_ID = ["button1", "button2"]
 AIO_USERNAME = "minhnguyen2810"
-AIO_KEY = "aio_OtRr52oAZyTiFTJu2pSJlgr6ekeR"
+AIO_KEY = "aio_zkmX68TybVJ5T88gzLQnj38pXdrG"
 
 def connected(client):
     print("Ket noi thanh cong ...")
     for topic in AIO_FEED_ID:
-        client.subscribe(AIO_FEED_ID)
+        client.subscribe(topic)
 
 def subscribe(client , userdata , mid , granted_qos):
     print("Subscribe thanh cong ...")
@@ -20,7 +20,7 @@ def disconnected(client):
     sys.exit (1)
 
 def message(client , feed_id , payload):
-    print("Nhan du lieu: " + payload)
+    print("Nhan du lieu: " + payload, "feed id: " + feed_id)
 
 client = MQTTClient(AIO_USERNAME , AIO_KEY)
 client.on_connect = connected
@@ -31,17 +31,25 @@ client.connect()
 client.loop_background()
 
 counter = 10
+sensor_type = 0
 while True:
     counter = counter-1
     if counter<=0:
         counter = 10
-        print("")
-        print("random data is publishing....")
-        temp = random.randint(10,20)
-        client.publish("demo", temp)
-        humi = random.randint(19,70)
-        client.publish("demo1", humi)
-        light = random.randint(10, 50)
-        client.publish("demo2", light)
+        if(sensor_type==0):
+            print("temperature data is publishing....")
+            temp = random.randint(10,20)
+            client.publish("demo", temp)
+            sensor_type = 1
+        elif(sensor_type==1): 
+            print("humidity data is publishing....")
+            humi = random.randint(19,70)
+            client.publish("demo1", humi)
+            sensor_type = 2
+        elif(sensor_type==2): 
+            print("light data is publishing....")
+            light = random.randint(10, 50)
+            client.publish("demo2", light)
+            sensor_type = 0
     time.sleep(1)
     pass
